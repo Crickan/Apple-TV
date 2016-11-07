4073342eb0508ac7c09d247d5c3514416f4de5c2
swttt <bas@MacBook-van-Bas.fritz.box>
2016-11-07 20:20:16 +0100

    Update for non use of default port

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ function init() {
 	Homey.log("Starting airplay client");
 
 	bonjour.find({ type: 'airplay' }, function (service) {
-	  console.log('Found an AirPlay server:', service);
-		var airplay = new AirPlay(service.referer.address);
+	  //console.log('Found an AirPlay server:', service);
+		var airplay = new AirPlay(service.referer.address, service.port);
 		airplay.play('http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4', function (err) {
 		  if (err) throw err
 
 		  airplay.playbackInfo(function (err, res, body) {
 		    if (err) throw err
-		    console.log('Playback info:', body)
+		    //console.log('Playback info:', body)
 		  })
 		})
 	});
